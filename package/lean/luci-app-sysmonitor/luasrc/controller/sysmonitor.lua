c7b9f04bd594e3e82b94d7573719ba690afa9449
sofueduscn <softeduscn@gmail.com>
2022-05-23 07:06:05 +0800

    Update

--- package/lean/luci-app-sysmonitor/luasrc/controller/sysmonitor.lua
+++ package/lean/luci-app-sysmonitor/luasrc/controller/sysmonitor.lua
@@ -11,21 +11,28 @@ function index()
 	entry({"admin", "sys","sysmonitor"}, alias("admin", "sys","sysmonitor", "settings"),_("SYSMonitor"), 20).dependent = true
 	entry({"admin", "sys", "sysmonitor", "settings"}, cbi("sysmonitor/setup"),_("General Settings"), 30).leaf = true
 --	entry({"admin", "sys", "sysmonitor", "update"}, form("sysmonitor/filetransfer"),_("Update"), 40).leaf = true
---	entry({"admin", "sys", "sysmonitor", "log"},cbi("sysmonitor/log"),_("Log"), 50).leaf = true
+--	entry({"admin", "sys", "sysmonitor", "wgusers"},cbi("sysmonitor/wgusers"),_("WGusers"), 50).leaf = true
+--	entry({"admin", "sys", "sysmonitor", "log"},cbi("sysmonitor/log"),_("Log"), 60).leaf = true
 
 	entry({"admin", "sys", "sysmonitor", "ipsecfw_status"}, call("action_ipsecfw_status")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "gateway_status"}, call("action_gateway_status")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "vpn_status"}, call("action_vpn_status")).leaf = true
+	entry({"admin", "sys", "sysmonitor", "wireguard_status"}, call("action_wireguard_status")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "ipsec_status"}, call("action_ipsec_status")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "pptp_status"}, call("action_pptp_status")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "switch_vpn"}, call("switch_vpn")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "switch_ipsecfw"}, call("switch_ipsecfw")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "onoff_vpn"}, call("onoff_vpn")).leaf = true
 	entry({"admin", "sys", "sysmonitor", "get_log"}, call("get_log")).leaf = true
+	entry({"admin", "sys", "sysmonitor", "wg_users"}, call("wg_users")).leaf = true
 end
 
 function get_log()
 	luci.http.write(luci.sys.exec("[ -f '/var/log/sysmonitor.log' ] && cat /var/log/sysmonitor.log"))
+end
+
+function wg_users()
+	luci.http.write(luci.sys.exec("[ -f '/var/log/wg_users' ] && cat /var/log/wg_users"))
 end
 
 function get_users()
@@ -51,6 +58,20 @@ function action_vpn_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		vpn_state = luci.sys.exec("/usr/share/sysmonitor/sysapp.sh ssr")
+	})
+end
+
+function action_wireguard_status()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		wireguard_state = luci.sys.exec("/usr/share/sysmonitor/sysapp.sh wg")
+	})
+end
+
+function action_wg_status()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		wg_state = luci.sys.exec("curl http://47.100.183.141/getwg.php")
 	})
 end
 
